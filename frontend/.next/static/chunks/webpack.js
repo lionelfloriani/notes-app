f72c6fd99a19e663aa84f9ec23a3c86cabc11d77
Lionel Floriani <lionel.floriani@outlook.com>
2023-06-09 17:04:16 +0200

    base

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "039065500ee60348"; }
+/******/ 		__webpack_require__.h = function() { return "b8f01ac9f917f43d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/pages/components/FullNote.js
+++ frontend/pages/components/FullNote.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-function FullNote({ data, createdAt, isEditing, editedNote, onEdit, onSave }) {
+function FullNote({ data, createdAt, onUpdatedNote }) {
+  const [isEditing, setIsEditing] = useState(false)
+  const [editedNote, setEditedNote] = useState({})
+
+  useEffect(() => {
+    setEditedNote({ ...data })
+  }, [data])
+
   const handleSaveNote = () => {
-    onSave(editedNote)
+    setIsEditing(false)
+    // Make an API call to update the note
+    fetch(`http://localhost:5000/api/v1/notes/${data._id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(editedNote),
+    })
+      .then((response) => {
+        if (response.ok) {
+          // Note updated successfully, update the state in the parent component
+          onUpdatedNote(data._id, editedNote)
+          console.log('Note updated successfully')
+        } else {
+          console.error('Error updating note:', response.statusText)
+          // Handle the error case if needed
+        }
+      })
+      .catch((error) => {
+        console.error('Error updating note:', error)
+        // Handle the error case if needed
+      })
   }
 
   return (
@@ -11,17 +40,17 @@ function FullNote({ data, createdAt, isEditing, editedNote, onEdit, onSave }) {
         <input
           type="text"
           value={editedNote.title}
-          onChange={(e) => onEdit({ ...editedNote, title: e.target.value })}
+          onChange={(e) => setEditedNote({ ...editedNote, title: e.target.value })}
           className="text-2xl font-bold mb-4"
         />
       ) : (
         <h1 className="text-2xl font-bold mb-4">{data.title}</h1>
       )}
-      <p className="text-gray-500 text-xs mb-2">{createdAt}</p>
+      <p className="text-gray-500 text-xs mb-2">{data.createdAt}</p>
       {isEditing ? (
         <textarea
           value={editedNote.content}
-          onChange={(e) => onEdit({ ...editedNote, content: e.target.value })}
+          onChange={(e) => setEditedNote({ ...editedNote, content: e.target.value })}
           className="flex-1 bg-transparent outline-none"
         />
       ) : (
@@ -33,7 +62,7 @@ function FullNote({ data, createdAt, isEditing, editedNote, onEdit, onSave }) {
             Save
           </button>
         ) : (
-          <button onClick={() => onEdit(data)}>Edit</button>
+          <button onClick={() => setIsEditing(true)}>Edit</button>
         )}
       </div>
     </div>
