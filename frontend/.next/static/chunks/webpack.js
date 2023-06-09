8390abcf9dfcef196b03fe5a6a187a55be5bcd96
Lionel Floriani <lionel.floriani@outlook.com>
2023-06-09 16:53:35 +0200

    edit and save implemented but need fixes

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "05068c6c32985240"; }
+/******/ 		__webpack_require__.h = function() { return "039065500ee60348"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
